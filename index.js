126294bb90635d6b8d38dded3a5e13d4163d7752
Sara Rizzardini <rizzardinis777@gmail.com>
2024-12-11 13:31:02 +0000

    second commit

--- index.js
+++ index.js
@@ -23,9 +23,15 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
-
-
+const date= new Date();
+const dateUtc= date.toUTCString();
+const unix = date.getTime();
+app.get("/api/{date}", function (req, res) {
+  res.json({unix: unix, utc:dateUtc});
+});
+app.get("/api/{unix}", function (req, res) {
+  res.json({unix: unix, utc:dateUtc});
+});
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
